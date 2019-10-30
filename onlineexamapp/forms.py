949588f3bb4ad25b54d9f21ef0fc2f83e6ffb86d
maimbeeb <mariamhabib1@hotmail.com>
2019-10-31 00:20:34 +0530

    Forms added related to Apply for Exams

--- onlineexamapp/forms.py
+++ onlineexamapp/forms.py
@@ -9,4 +9,20 @@ class UserRegister(forms.Form):
     upwd = forms.CharField(label='', widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'Password'}))
     ucpwd = forms.CharField(label='', widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))    
+        attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+
+class UserLogin(forms.Form):
+    uemail = forms.EmailField(label='', widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    upwd = forms.CharField(label='', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password'}))  
+
+class UserForget(forms.Form):
+    uemail = forms.EmailField(label='', widget=forms.EmailInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email'}))       
+
+class UserChangePwd(forms.Form):
+    upwd = forms.CharField(label='', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Old Password'}))
+    unpwd = forms.CharField(label='', widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'New Password'}))        
