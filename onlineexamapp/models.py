eef5e6aab584303074062c934a64d8d576f9ff75
maimbeeb <mariamhabib1@hotmail.com>
2019-10-31 00:19:20 +0530

    New Model added for User-Exams

--- onlineexamapp/models.py
+++ onlineexamapp/models.py
@@ -6,4 +6,16 @@ class User(models.Model):
     uemail = models.EmailField()  
     upwd = models.CharField(max_length=20)  
     class Meta:  
-        db_table = "user" 
+        db_table = "user"
+
+class UserExam(models.Model):
+    eno = models.CharField(max_length=50)  
+    uemail = models.EmailField()  
+    uname = models.CharField(max_length=100)
+    exname = models.CharField(max_length=150)  
+    amount = models.IntegerField()
+    currency = models.CharField(max_length=10)
+    stripetoken = models.TextField()
+    charge_response = models.TextField()
+    class Meta:  
+        db_table = "user_exams"        
